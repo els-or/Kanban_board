f12005cd93352eaa6e7a8df9bcf4c93cd8944663
Rachel Sutton <raesak@gmail.com>
2025-03-04 13:46:08 -0800

    server setup

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -1,15 +1,60 @@
-import { Router, Request, Response } from 'express';
+import { Router, type Request, type Response } from 'express';
 import { User } from '../models/user.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
-export const login = async (req: Request, res: Response) => {
-  // TODO: If the user exists and the password is correct, return a JWT token
+//register a user.
+export const register = async (req: Request, res: Response) => {
+  const { username, password } = req.body; //getting data from client.
+  //check if username already exists.
+  const existingUser = await User.findOne({
+    where: { username },
+  });
+  if (existingUser) {
+    return res.status(409).json({ message: 'Username already exists' });
+  }
+  //create a new user in the database.
+  try {
+    await User.create({
+      username,
+      password,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: 'Error creating user' });
+  }
+  return res.status(201).json({ message: 'User created successfully' });
 };
 
-const router = Router();
+//login server.
+export const login = async (req: Request, res: Response) => {
+  const { username, password } = req.body;  //getting data from client.
+
+// store result of query.
+  const user = await User.findOne({   //method to query a record.
+    where: { username },  // where username matches username variable.
+  });
+  if (!user) {
+    return res.status(401).json({ message: 'Authentication failed' });
+  }
+
+  //compare input password to the store password in a database.
+  const passwordIsValid = await bcrypt.compare(password, user.password); 
+  if (!passwordIsValid) {
+    return res.status(401).json({ message: 'Authentication failed' });
+  }
+
+  const secretKey = process.env.JWT_SECRET_KEY || '';
+
+  //create token and return to client.
+  const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+  return res.json({ token });
+};
+
+const router = Router(); //create new instance of Router()
 
 // POST /login - Login a user
 router.post('/login', login);
 
+router.post('/register', register)
 export default router;
