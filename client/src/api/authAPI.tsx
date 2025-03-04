09b81df2da85f8a428fc24d0f4d9fca25afad731
Rachel Sutton <raesak@gmail.com>
2025-03-04 13:46:16 -0800

    client setup

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,9 +1,29 @@
-import { UserLogin } from "../interfaces/UserLogin";
+import type { UserLogin } from "../interfaces/UserLogin";
+
+//login client.
 
 const login = async (userInfo: UserLogin) => {
-  // TODO: make a POST request to the login route
-}
+  try {
+    const response = await fetch("/auth/login", {
+      // starting a network rewquet to endpoint using a post requst.
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json", // hold JSON data.
+      },
+      body: JSON.stringify(userInfo), //convert to JSON string.
+    });
 
+    const data = await response.json(); //store response object.
 
+    if (!response.ok) {
+      throw new Error("User information not retrieved, check network tab!");
+    }
+
+    return data;
+  } catch (err) {
+    console.log("Error from user login: ", err);
+    return Promise.reject("Could not fetch user info");
+  }
+};
 
 export { login };
